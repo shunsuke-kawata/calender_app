0f9d30bc9c3c22b02afc798185e1a569e64e39ff
shunsuke-kawata <kwt20238120@gmail.com>
2025-04-29 21:41:47 +0900

    update タイトルの変更

--- frontend_react/src/components/header/header.tsx
+++ frontend_react/src/components/header/header.tsx
@@ -1,14 +1,18 @@
 import "./header.css";
 import HeaderIcon from "../headerIcon/headerIcon";
+import { useNavigate } from "react-router-dom";
 
 interface HeaderProps {
   title: string;
 }
 
 const Header: React.FC<HeaderProps> = ({ title }) => {
+  const navigate = useNavigate();
   return (
     <div className="header-container">
-      <div className="app-title">中山研カレンダー</div>
+      <div className="app-title" onClick={() => navigate("/")}>
+        中山研カレンダー
+      </div>
       <div className="page-title">{title}</div>
       <HeaderIcon />
     </div>
--- frontend_react/src/pages/topPage/topPage.tsx
+++ frontend_react/src/pages/topPage/topPage.tsx
@@ -2,7 +2,7 @@ import Header from "../../components/header/header";
 const TopPage: React.FC = () => {
   return (
     <>
-      <Header title="スケジュール" />
+      <Header title="ホーム" />
     </>
   );
 };
